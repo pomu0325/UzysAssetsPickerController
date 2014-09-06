368cbfe9103d44a53a20af2b04662b991f8a5ac8
pomu0325 <pomu0325@gmail.com>
2014-09-06 21:35:06 +0900

    use lowercase method name for swift support

--- UzysAssetsPickerController/Library/UzysAssetsPickerController.m
+++ UzysAssetsPickerController/Library/UzysAssetsPickerController.m
@@ -554,8 +554,8 @@
 - (BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath
 {
     BOOL didExceedMaximumNumberOfSelection = [collectionView indexPathsForSelectedItems].count >= self.maximumNumberOfSelection;
-    if (didExceedMaximumNumberOfSelection && self.delegate && [self.delegate respondsToSelector:@selector(UzysAssetsPickerControllerDidExceedMaximumNumberOfSelection:)]) {
-        [self.delegate UzysAssetsPickerControllerDidExceedMaximumNumberOfSelection:self];
+    if (didExceedMaximumNumberOfSelection && self.delegate && [self.delegate respondsToSelector:@selector(uzysAssetsPickerControllerDidExceedMaximumNumberOfSelection:)]) {
+        [self.delegate uzysAssetsPickerControllerDidExceedMaximumNumberOfSelection:self];
     }
     return !didExceedMaximumNumberOfSelection;
 }
@@ -586,8 +586,8 @@
     {
         UzysAssetsPickerController *picker = (UzysAssetsPickerController *)self;
         
-        if([picker.delegate respondsToSelector:@selector(UzysAssetsPickerController:didFinishPickingAssets:)])
-            [picker.delegate UzysAssetsPickerController:picker didFinishPickingAssets:assets];
+        if([picker.delegate respondsToSelector:@selector(uzysAssetsPickerController:didFinishPickingAssets:)])
+            [picker.delegate uzysAssetsPickerController:picker didFinishPickingAssets:assets];
         
         [self dismissViewControllerAnimated:YES completion:^{
             
@@ -828,9 +828,9 @@
             break;
         case kTagButtonClose:
         {
-            if([self.delegate respondsToSelector:@selector(UzysAssetsPickerControllerDidCancel:)])
+            if([self.delegate respondsToSelector:@selector(uzysAssetsPickerControllerDidCancel:)])
             {
-                [self.delegate UzysAssetsPickerControllerDidCancel:self];
+                [self.delegate uzysAssetsPickerControllerDidCancel:self];
             }
             [self dismissViewControllerAnimated:YES completion:^{
                 
